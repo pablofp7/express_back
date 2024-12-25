4e9a3aafa79af3c2f3fccee893dd683afe17b15c
pablofp7 <pablofernandezpi@gmail.com>
2024-12-25 13:37:22 +0100

    Archivo de testing para movie controller finalizado correctamente

--- tests/unit/controllers/movieController.test.js
+++ tests/unit/controllers/movieController.test.js
@@ -145,4 +145,143 @@ describe('MovieController', () => {
       expect(validateMovieStub.args[0][0]).to.deep.equal(req.body) // Verifica los argumentos
     })
   })
+
+  describe('delete', () => {
+    it('debería eliminar correctametne una película por ID', async () => {
+      req.params.id = '1'
+
+      checkUUIDStub.resolves(true) // Simula que el UUID es válido
+      movieModelMock.delete.resolves(true) // Simula que la película fue eliminada
+
+      await movieController.delete(req, res, next)
+
+      expect(checkUUIDStub.calledOnceWith('1')).to.be.true
+      expect(movieModelMock.delete.calledOnceWith({ id: '1' })).to.be.true
+      expect(res.status.calledOnceWith(200)).to.be.true
+      expect(res.json.calledOnceWith({ message: 'Movie deleted' })).to.be.true
+    }),
+    it('debería lanzar un CustomError si el ID de la película no es válido', async () => {
+      req.params.id = 'Invalid ID'
+      checkUUIDStub.resolves(false) // Simula que el UUID no es válido
+      movieModelMock.delete.resolves(true) // Simula que la película fue eliminada
+
+      const createPromise = movieController.delete(req, res, next)
+
+      await expect(createPromise).to.be.rejectedWith(CustomError)
+      await expect(createPromise).to.be.rejectedWith(ERROR_TYPES.general.INVALID_UUID)
+
+      expect(checkUUIDStub.calledOnceWith('Invalid ID')).to.be.true
+      expect(movieModelMock.delete.called).to.be.false
+    })
+  }),
+  it('debería lanzar un CustomError si la película no existe', async () => {
+    req.params.id = '1'
+
+    checkUUIDStub.resolves(true) // Simula que el UUID es válido
+    movieModelMock.delete.resolves({ affectedRows: 0 }) // Simula que no se encuentra la película
+
+    const createPromise = movieController.delete(req, res, next)
+
+    await expect(createPromise).to.be.rejectedWith(CustomError)
+    await expect(createPromise).to.be.rejectedWith(ERROR_TYPES.movie.NOT_FOUND)
+
+    expect(checkUUIDStub.calledOnceWith('1')).to.be.true
+    expect(movieModelMock.delete.calledOnceWith({ id: '1' })).to.be.true
+  }),
+
+  describe('update', () => {
+    it('debería actualizar una película con datos válidos', async () => {
+      req.params.id = '1'
+      req.body = { title: 'Updated Title', genre: 'Drama' } // Datos válidos
+      const mockResult = { affectedRows: 1 }
+
+      checkUUIDStub.resolves(true) // UUID válido
+      validatePartialMovieStub.resolves(true) // Validación exitosa
+      movieModelMock.update.resolves(mockResult) // Simula que se actualizó la película
+
+      await movieController.update(req, res, next)
+
+      expect(checkUUIDStub.calledOnceWith('1')).to.be.true // Verifica que se validó el UUID
+      expect(validatePartialMovieStub.calledOnceWith(req.body)).to.be.true // Verifica la validación
+      expect(movieModelMock.update.calledOnceWith({
+        id: '1',
+        fields: [['title', 'Updated Title']],
+        genre: 'Drama',
+      })).to.be.true // Verifica la actualización
+      expect(res.status.calledOnceWith(200)).to.be.true // Código de estado correcto
+      expect(res.json.calledOnceWith({ message: 'Movie updated successfully' })).to.be.true // Respuesta correcta
+    })
+
+    it('debería lanzar un CustomError si el UUID es inválido', async () => {
+      req.params.id = 'invalid-uuid'
+      req.body = { title: 'Updated Title', genre: 'Drama' }
+
+      checkUUIDStub.resolves(false) // UUID inválido
+
+      const updatePromise = movieController.update(req, res, next)
+
+      await expect(updatePromise).to.be.rejectedWith(CustomError)
+      await expect(updatePromise).to.be.rejectedWith(ERROR_TYPES.general.INVALID_UUID)
+
+      expect(checkUUIDStub.calledOnceWith('invalid-uuid')).to.be.true
+      expect(validatePartialMovieStub.called).to.be.false // Validación no debería ser llamada
+      expect(movieModelMock.update.called).to.be.false // No debería llamar al modelo
+    })
+
+    it('debería lanzar un CustomError si los datos son inválidos', async () => {
+      req.params.id = '1'
+      req.body = { title: '', genre: '' } // Datos inválidos
+
+      checkUUIDStub.resolves(true) // UUID válido
+      validatePartialMovieStub.resolves(false) // Validación fallida
+
+      const updatePromise = movieController.update(req, res, next)
+
+      await expect(updatePromise).to.be.rejectedWith(CustomError)
+      await expect(updatePromise).to.be.rejectedWith(ERROR_TYPES.movie.VALIDATION_ERROR)
+
+      expect(checkUUIDStub.calledOnceWith('1')).to.be.true
+      expect(validatePartialMovieStub.calledOnceWith(req.body)).to.be.true // Validación intentada
+      expect(movieModelMock.update.called).to.be.false // No debería llamar al modelo
+    })
+
+    it('debería lanzar un CustomError si no se proporcionan campos válidos para actualizar', async () => {
+      req.params.id = '1'
+      req.body = { invalidField: 'Invalid' } // Ningún campo permitido
+
+      checkUUIDStub.resolves(true) // UUID válido
+      validatePartialMovieStub.resolves(true) // Validación exitosa
+
+      const updatePromise = movieController.update(req, res, next)
+
+      await expect(updatePromise).to.be.rejectedWith(CustomError)
+      await expect(updatePromise).to.be.rejectedWith(ERROR_TYPES.movie.VALIDATION_ERROR)
+
+      expect(checkUUIDStub.calledOnceWith('1')).to.be.true
+      expect(validatePartialMovieStub.calledOnceWith(req.body)).to.be.true
+      expect(movieModelMock.update.called).to.be.false // No debería llamar al modelo
+    })
+
+    it('debería lanzar un CustomError si la película no existe', async () => {
+      req.params.id = '1'
+      req.body = { title: 'Updated Title', genre: 'Drama' } // Datos válidos
+
+      checkUUIDStub.resolves(true) // UUID válido
+      validatePartialMovieStub.resolves(true) // Validación exitosa
+      movieModelMock.update.resolves({ affectedRows: 0 }) // Película no encontrada
+
+      const updatePromise = movieController.update(req, res, next)
+
+      await expect(updatePromise).to.be.rejectedWith(CustomError)
+      await expect(updatePromise).to.be.rejectedWith(ERROR_TYPES.movie.NOT_FOUND)
+
+      expect(checkUUIDStub.calledOnceWith('1')).to.be.true
+      expect(validatePartialMovieStub.calledOnceWith(req.body)).to.be.true
+      expect(movieModelMock.update.calledOnceWith({
+        id: '1',
+        fields: [['title', 'Updated Title']],
+        genre: 'Drama',
+      })).to.be.true // Intentó actualizar
+    })
+  })
 })
