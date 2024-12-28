85f7d70dc1036d94c357f644c5d3a339e21453ed
pablofp7 <pablofernandezpi@gmail.com>
2024-12-28 13:55:13 +0100

    Updating eslint config to making it look less redundant.

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,8 @@ import validErrorCodesPlugin from './myEslintRules/validErrorCodes.js'
 import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
+  // Common configuration shared by all files
   {
-
     files: ['**/*.js'],
     ignores: ['node_modules', 'dist', 'tests/**'],
     languageOptions: {
@@ -17,7 +17,6 @@ export default [
         require: 'readonly',
         module: 'readonly',
         Buffer: 'readonly',
-
       },
     },
     plugins: {
@@ -27,7 +26,6 @@ export default [
     },
     rules: {
       ...stylistic.configs['recommended-flat'].rules,
-
       'prefer-const': 'error',
       'no-var': 'error',
       'object-shorthand': ['error', 'always'],
@@ -35,7 +33,6 @@ export default [
       '@stylistic/arrow-parens': 'off',
       'eqeqeq': ['error', 'always'],
       'no-undef': 'error',
-
       'no-unused-vars': 'off',
       'unused-imports/no-unused-vars': ['error', {
         vars: 'all',
@@ -56,16 +53,13 @@ export default [
         ignoreRegExpLiterals: true,
       }],
       'prefer-template': 'error',
-
       'valid-error-structure/valid-error-structure': 'error',
     },
   },
-
+  // Overrides for test files
   {
     files: ['tests/**/*.js'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
       globals: {
         describe: 'readonly',
         it: 'readonly',
@@ -73,52 +67,11 @@ export default [
         beforeEach: 'readonly',
         after: 'readonly',
         afterEach: 'readonly',
-        console: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        require: 'readonly',
-        module: 'readonly',
-        Buffer: 'readonly',
         setImmediate: 'readonly',
       },
     },
-    plugins: {
-      '@stylistic': stylistic,
-      'valid-error-structure': validErrorCodesPlugin,
-      'unused-imports': unusedImports,
-    },
     rules: {
-      ...stylistic.configs['recommended-flat'].rules,
-      'prefer-const': 'error',
-      'no-var': 'error',
-      'object-shorthand': ['error', 'always'],
-      'arrow-parens': ['error', 'always'],
-      '@stylistic/arrow-parens': 'off',
-      'eqeqeq': ['error', 'always'],
-      'no-undef': 'error',
-      'no-unused-vars': 'off',
-      'unused-imports/no-unused-vars': ['error', {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_',
-      }],
-      'no-duplicate-imports': 'error',
-      'no-shadow': 'error',
-      'max-len': ['error', {
-        code: 300,
-        ignoreComments: true,
-        ignoreUrls: true,
-        ignoreStrings: false,
-        ignoreTemplateLiterals: false,
-        ignoreRegExpLiterals: true,
-      }],
-      'prefer-template': 'error',
-      'valid-error-structure/valid-error-structure': 'error',
-
-      'no-unused-expressions': 'off',
+      'no-unused-expressions': 'off', // Specific to test files
     },
   },
 ]
