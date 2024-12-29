c6aae0e8873ad1fdaf5be478aa677da7d513c6ce
pablofp7 <pablofernandezpi@gmail.com>
2024-12-29 20:38:55 +0100

    Small refactor to make easier the testing for the cors middleware.

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -7,26 +7,30 @@ const ACCEPTED_ORIGINS = [
   */
 ]
 
+// Export the custom origin function separately for testing
+export const originCallback = (origin, callback) => {
+  if (!origin) {
+    return callback(null, true)
+  }
+
+  if (origin.startsWith('http://localhost')) {
+    return callback(null, true)
+  }
+
+  if (ACCEPTED_ORIGINS.includes(origin)) {
+    return callback(null, true)
+  }
+
+  console.error(`Blocked by CORS: origin not allowed - ${origin}`)
+  return callback(
+    new CustomError({
+      origError: new Error(`Blocked by CORS: ${origin}`),
+      errorType: ERROR_TYPES.auth.ACCESS_DENIED,
+    }),
+  )
+}
+
+// Use the custom originCallback in the cors middleware
 export const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    if (origin.startsWith('http://localhost')) {
-      return callback(null, true)
-    }
-
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    console.error(`Blocked by CORS: origin not allowed - ${origin}`)
-    return callback(
-      new CustomError({
-        origError: new Error(`Blocked by CORS: ${origin}`),
-        errorType: ERROR_TYPES.auth.ACCESS_DENIED,
-      }),
-    )
-  },
+  origin: originCallback,
 })
