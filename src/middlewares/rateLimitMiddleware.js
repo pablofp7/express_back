efa6db6552726a5983d81fb84eeade9b0a5a8a9c
pablofp7 <pablofernandezpi@gmail.com>
2024-12-30 15:07:38 +0100

    Exposing limiter's handlers for unitary testing.

--- src/middlewares/rateLimitMiddleware.js
+++ src/middlewares/rateLimitMiddleware.js
@@ -2,28 +2,32 @@ import rateLimit from 'express-rate-limit'
 import { blacklist } from '../utils/blacklist.js'
 import { CustomError, ERROR_TYPES } from '../errors/customError.js'
 
+export const generalLimiterHandler = (req, res, next) => {
+  const error = new CustomError({
+    origError: new Error('Too many requests'),
+    errorType: ERROR_TYPES.general.TOO_MANY_REQUESTS,
+  })
+  blacklist.add(req.ip)
+  next(error)
+}
+
+export const sensitiveLimiterHandler = (req, res, next) => {
+  const error = new CustomError({
+    origError: new Error('Too many requests on sensitive route'),
+    errorType: ERROR_TYPES.general.TOO_MANY_REQUESTS,
+  })
+  blacklist.add(req.ip)
+  next(error)
+}
+
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  handler: (req, res, next) => {
-    const error = new CustomError({
-      origError: new Error('Too many requests'),
-      errorType: ERROR_TYPES.general.TOO_MANY_REQUESTS,
-    })
-    blacklist.add(req.ip)
-    next(error)
-  },
+  handler: generalLimiterHandler,
 })
 
 export const sensitiveLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 10,
-  handler: (req, res, next) => {
-    const error = new CustomError({
-      origError: new Error('Too many requests on sensitive route'),
-      errorType: ERROR_TYPES.general.TOO_MANY_REQUESTS,
-    })
-    blacklist.add(req.ip)
-    next(error)
-  },
+  handler: sensitiveLimiterHandler,
 })
