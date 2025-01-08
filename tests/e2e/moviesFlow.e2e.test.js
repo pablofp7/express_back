7f9a8b75ed841c53fe9e7028c01bcd2770645f2a
pablofp7 <pablofernandezpi@gmail.com>
2025-01-08 20:01:03 +0100

    Testing if the workflow works

--- tests/e2e/moviesFlow.e2e.test.js
+++ tests/e2e/moviesFlow.e2e.test.js
@@ -7,12 +7,12 @@ import supertest from 'supertest'
 import { expect } from 'chai'
 import { config } from '../../src/config/config.js'
 
-let request, dbConn
+let _request, dbConn
 
 before(async () => {
   const { appInstance, dbConn: connections } = await generateAppInstance()
   dbConn = connections
-  request = supertest(appInstance)
+  _request = supertest(appInstance)
 })
 
 after(async () => {
@@ -41,3 +41,51 @@ console.log(`
 `)
 
 describe('E2E Tests for Movies Routes', () => {
+
+})
+
+async function _cleanupTestUser() {
+  try {
+    await dbConn.userConn.query({
+      query: `
+        DELETE FROM user_roles
+        WHERE user_id IN (
+          SELECT id FROM user WHERE username = ?
+        )
+      `,
+      queryParams: ['testUser'],
+    })
+
+    await dbConn.userConn.query({
+      query: 'DELETE FROM user WHERE username = ?',
+      queryParams: ['testUser'],
+    })
+  }
+  catch (error) {
+    console.error('Error cleaning up test user:', error)
+    throw error
+  }
+}
+
+async function generateAppInstance() {
+  try {
+    const movieModel = new MovieModel({ movieDbType: 'local' })
+    const userModel = new UserModel({ userDbType: 'local' })
+
+    await Promise.all([movieModel.init(), userModel.init()])
+
+    const appInstance = createApp({ movieModel, userModel })
+    return { appInstance, dbConn: { movieConn: movieModel.databaseConnection, userConn: userModel.databaseConnection } }
+  }
+  catch (error) {
+    if (error instanceof CustomError) {
+      throw error
+    }
+    else {
+      throw new CustomError({
+        origError: error,
+        errorType: ERROR_TYPES.general.SERVER_ERROR,
+      })
+    }
+  }
+}
